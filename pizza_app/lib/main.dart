e89323807144d8d1b9bf27904672a12443418bf9
Mutaaa <k.sangwichien@gmail.com>
2020-08-30 20:59:20 +0700

    Flutter app; select image from camera, gallery

---
+++ pizza_app/lib/main.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+                title: Text('Pizza_app made by A+'),
+                backgroundColor: Colors.transparent
+            ),
+            body: Center(
+                child: MyImagePicker()
+            )
+        )
+    );
+  }
+}
+
+
+
+class MyImagePicker extends StatefulWidget {
+  @override
+  MyImagePickerState createState() => MyImagePickerState();
+}
+
+class MyImagePickerState extends State {
+  File imageURI;
+  String result;
+  String path;
+  Future getImageFromCamera() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+    setState(() {
+      imageURI = image;
+      path = image.path;
+    });
+  }
+
+  Future getImageFromGallery() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      imageURI = image;
+      path = image.path;
+    });
+  }
+
+//  Future classifyImage() async {
+
+//  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  imageURI == null
+                      ? Text('No image selected.')
+                      : Image.file(imageURI, width: 300, height: 200, fit: BoxFit.cover),
+                  Container(
+                      margin: EdgeInsets.fromLTRB(0, 30, 0, 20),
+                      child: RaisedButton(
+                        onPressed: () => getImageFromCamera(),
+                        child: Text('Click Here To Select Image From Camera'),
+                        textColor: Colors.white,
+                        color: Colors.blue,
+                        padding: EdgeInsets.fromLTRB(12, 12, 12, 12),
+                      )),
+                  Container(
+                      margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                      child: RaisedButton(
+                        onPressed: () => getImageFromGallery(),
+                        child: Text('Click Here To Select Image From Gallery'),
+                        textColor: Colors.white,
+                        color: Colors.blue,
+                        padding: EdgeInsets.fromLTRB(12, 12, 12, 12),
+                      )),
+                  Container(
+                      margin: EdgeInsets.fromLTRB(0, 30, 0, 20),
+                      child: RaisedButton(
+                        //onPressed: () => classifyImage(),
+                        child: Text('Classify Image'),
+                        textColor: Colors.white,
+                        color: Colors.blue,
+                        padding: EdgeInsets.fromLTRB(12, 12, 12, 12),
+                      )),
+                  result == null
+                      ? Text('Result')
+                      : Text(result)
+                ]))
+    );
+  }
+
+}
